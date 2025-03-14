edfd94cbe27476086fd356d1463a5024c1bb4fa6
Abraham Romero <romero9804@hotmail.com>
2025-03-14 14:51:51 -0700

    fix: corregir error en redireccion al registrar usuario
    se corrigio el error al redirigir al usuario despues de registrarse agregando como parametro el nombre de usuario

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -36,6 +36,6 @@ class RegisterController extends Controller
         Auth::attempt($request->only('email','password'));
 
 
-        return redirect()->route('posts.index');
+        return redirect()->route('posts.index', Auth::user()->username);
     }
 }
